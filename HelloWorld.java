e3d09f83e394a787758538936de2bac6cfef3ee2
CHssmith2 <ssmith2@companieshouse.gov.uk>
2021-12-02 12:53:36 +0000

    everted back to Hello World!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("I Love Conflicts");
-		System.out.println("This is so much fun!");
+		System.out.println("Hello World!");
 	}
 }
