5d2364b50649f1e92c90289cd6e71e307abb5b42
CHssmith2 <ssmith2@companieshouse.gov.uk>
2021-12-02 13:44:24 +0000

    Changed the println statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("I Love Conflicts");
+		System.out.println("I Absolutely Love Conflicts");
 		System.out.println("This is so much fun!");
 	}
 }
